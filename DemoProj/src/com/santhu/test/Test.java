8cbff79dadee96c02389899e5a11dda750c1ca38
santhosh <santhoshty97@gmail.com>
2020-12-23 15:14:38 +0530

    dev1 modified Test.java

--- DemoProj/src/com/santhu/test/Test.java
+++ DemoProj/src/com/santhu/test/Test.java
@@ -1,5 +1,10 @@
 package com.santhu.test;
 
-public class Test {
+import com.santhu.beans.Utility;
 
+public class Test {
+	public static void main(String[] args) {
+		Utility utility = new Utility();
+		System.out.println(utility.getSysDate());
+	}
 }
